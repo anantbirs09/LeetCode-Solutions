e0759e10efa4365df7bf25879557b8bd020cacb5
Anantbir Singh <anantbirs09@gmail.com>
2026-06-15 18:03:47 +0530

    Edit

--- Medium/Car.java
+++ Medium/Car.java
@@ -1,6 +1,22 @@
 class Solution {
     public boolean carPooling(int[][] trips, int capacity) {
-        return ((trips[0][0]+trips[1][0]) <= capacity);
-  
+        
+        for (int i = 0; i < trips.length; i++) {
+            
+            int currentPassengers = 0;
+            
+            for (int j = 0; j < trips.length; j++) {
+                
+                if (trips[j][1] <= trips[i][1] && trips[j][2] > trips[i][1]) {
+                    currentPassengers += trips[j][0];
+                }
+            }
+            
+            if (currentPassengers > capacity) {
+                return false;
+            }
+        }
+        
+        return true;
     }
 }
